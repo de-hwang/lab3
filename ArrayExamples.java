33c6af91fb0d82613b30dafcbd77e9c79d83c0f4
de-hwang <daniel.eghwang@gmail.com>
2023-04-19 17:52:30 -0700

    Revert "Update ArrayTests.java"
    This reverts commit 3c2cd1deed13665a2bc71137bec01544a8f1a916.

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -4,10 +4,8 @@ public class ArrayExamples {
 
   // Changes the input array to be in reversed order
   static void reverseInPlace(int[] arr) {
-    for(int i = 0; i < arr.length/2; i += 1) {
-      int temp = arr[i];
+    for(int i = 0; i < arr.length; i += 1) {
       arr[i] = arr[arr.length - i - 1];
-      arr[arr.length - i - 1] = temp;
     }
   }
 
@@ -16,9 +14,9 @@ public class ArrayExamples {
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
-      newArray[i] = arr[arr.length - i - 1];
+      arr[i] = newArray[arr.length - i - 1];
     }
-    return newArray;
+    return arr;
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
