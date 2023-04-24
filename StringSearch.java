fc82b649120bff34a8d03bf79a4615922558aa52
de-hwang <daniel.eghwang@gmail.com>
2023-04-23 22:33:45 -0700

    Created StringSearch.java

---
+++ StringSearch.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.net.URI;
+
+class Handler implements URLHandler {
+    String concatStr = "";
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return """
+                Type \"/add-message?s=<string>\" to add messages
+                """;
+        } else if (url.getPath().contains("/add")) {
+            String[] parameters = url.getQuery().split("=");
+            if (parameters[0].equals("s")) {
+                concatStr = concatStr + parameters[1] + "\n";
+                return concatStr;
+            } else {
+                return "Invalid path!";
+            }
+        } 
+        return "404 Not Found!";
+    }
+}
+
+class StringSearch {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
